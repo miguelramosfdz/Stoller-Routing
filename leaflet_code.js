219621a8160cd817b98e6d9343d10cdee1e0ffd8
James <james.david.smith@gmail.com>
2013-02-10 12:49:42 +0000

    Update leaflet_code.js

--- leaflet_code.js
+++ leaflet_code.js
@@ -3,7 +3,7 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.cloudmade.com/10b29eb7b27e439e899b4352acbc86e3/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    attribution: 'Some attribution stuff here',
     maxZoom: 18
 }).addTo(map);
 
