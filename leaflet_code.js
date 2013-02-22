fd29e4a512de4cde7e405739d7f257e8fb1c3e34
James <james.david.smith@gmail.com>
2013-02-22 15:41:12 +0000

    Update leaflet_code.js

--- leaflet_code.js
+++ leaflet_code.js
@@ -1,6 +1,9 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-  	L.tileLayer('http://{s}.tile.cloudmade.com/10b29eb7b27e439e899b4352acbc86e3/997/256/{z}/{x}/{y}.png', {
+  	L.tileLayer('http://a.tiles.mapbox.com/v3/therealjimshady.map-3j8736fy/-0.11019999999999354,51.50380000000002,11/500x300.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 		}).addTo(map);
+
+
+http://{s}.tile.cloudmade.com/10b29eb7b27e439e899b4352acbc86e3/997/256/{z}/{x}/{y}.png
